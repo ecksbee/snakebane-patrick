a77e68384c9c3f79f6a65f9cb4a8bf065d9077e5
Joshua Nario <joshuanario@users.noreply.github.com>
2023-01-04 23:15:12 -0500

    Copyright

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import styles from './Footer.module.css'
 
 const Footer = (): JSX.Element => {
-    return <footer>
+    return <footer className={styles.footer}>
         <p className={styles.copyright}>
-        {"© 2022-" + new Date().getFullYear() + " ECKSBEE LLC. All rights reserved."}
+        {"© 2022-" + new Date().getFullYear() + " ECKSBEE LLC. All rights reserved.  All XBRL filings came directly from the U.S. Securities and Exchange Commission (SEC) and are publicly available through the SEC's EDGAR system."}
         </p>
     </footer>
 }
