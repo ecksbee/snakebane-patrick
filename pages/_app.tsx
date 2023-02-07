c2735a25ce662e46f62ac0af2e6ac8fd1ecab2ca
Joshua Nario <joshuanario@users.noreply.github.com>
2023-02-06 22:25:00 -0500

    edgarbrowser.com

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import CookieBanner from '../components/CookieBanner'
-import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Head>
-      <title>{`EDGARBrowser | ECKSBEE`}</title>
+      <title>{`EDGARBrowser`}</title>
     </Head>
     <Component {...pageProps} />
     <CookieBanner />
-    <Footer />
   </>
 }
--- pages/review/index.tsx
+++ pages/review/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Footer from '../../components/Footer'
 import logo from '../../public/logo.jpg'
 import styles from '../../styles/Review.module.css'
 
@@ -22,10 +23,10 @@ export default function Review() {
     renderer = <div><p>booting up renderer...</p></div>
   } else {
     renderer = id?  <iframe src={`/browser/?id=${id}`} style={{
-      position: 'fixed',
+      position: 'relative',
       left: '0',
       bottom: '0',
-      height: 'calc(100vh - 54px)',
+      height: 'calc(100vh - 54px - 20px)',
       width: '100vw',
       border: 'none'
     }}
@@ -33,17 +34,20 @@ export default function Review() {
     :
     <div><p>Unable to render XBRL filing.  The EDGAR filing may not have a well-formed XBRL report.  If there is a well-formed XBRL report, an error may have occurred.  There's a chance that hitting the browser's refresh/reload button to re-render will fix most errors.</p></div>
   }
-  return <div>
-    <h1 className={styles.Brand}>
-                <img src={logo.src} style={{verticalAlign: 'bottom'}} alt="ECKSBEE Logo" width={64.1} height={49.5} />
-                <a href="https://ecksbee.com" target="_blank">ECKSBEE</a> - EDGARBrowser
-    </h1>
-    <nav className={styles.Nav}>
-      <ul className={styles.NavList}>
-        <li><a href={`https://www.sec.gov/${path}`} target="_blank">Go to sec.gov</a></li>
-        <li><a href="/">Start over</a></li>
-      </ul>
-      </nav>
-      { renderer }
-  </div>
+  return <>
+    <div>
+      <h1 className={styles.Brand}>
+                  <img src={logo.src} style={{verticalAlign: 'bottom'}} alt="ECKSBEE Logo" width={64.1} height={49.5} />
+                  EDGARBrowser
+      </h1>
+      <nav className={styles.Nav}>
+        <ul className={styles.NavList}>
+          <li><a href={`https://www.sec.gov/${path}`} target="_blank">Go to sec.gov</a></li>
+          <li><a href="/">Start over</a></li>
+        </ul>
+        </nav>
+        { renderer }
+    </div>
+    <Footer short={true} />
+  </>
 }
