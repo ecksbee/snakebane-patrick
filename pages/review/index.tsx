8068e57f78550fade1ad727f498bd157b9070b78
Joshua Nario <joshuanario@users.noreply.github.com>
2023-02-07 22:07:46 -0500

    logo

--- pages/review/index.tsx
+++ pages/review/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import Footer from '../../components/Footer'
-import logo from '../../public/logo.jpg'
+import logo from '../../public/logo.png'
 import styles from '../../styles/Review.module.css'
 
 export default function Review() {
